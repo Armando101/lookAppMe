858d37c6fc8db1f95f2943d10c49def6620ec53c
Armando Rivera <rivera.armando997@gmail.com>
2021-02-26 13:40:18 -0600

    Carpeta de Griselda, se presentó un error debido a el historial de commit que es diferente

---
+++ js/user.js
@@ -0,0 +1,38 @@
+/*
+@author         Griselda Silvestre 
+@description    Clase que guarda los datos y métodos de un cliente
+@version        1.0
+*/
+class User{
+    constructor(firstName, lastName, birthdayDate, address, ocupation,type){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdayDate = birthdayDate;
+        this.address = address;
+        this.ocupation = ocupation;
+        this.type = type;
+        this.articles = {};
+    }
+    /*
+    @description    Método para actualizar la información del cliente 
+    */
+    updateInformation(){
+        //code
+    }
+
+    /*
+    @param      Artículo que se agrega  
+    */
+    addArticle(){
+        //code
+    }
+
+    /*
+    @param      Artículo que se elimina 
+    */
+    deleteArticle(){
+        //code
+    }
+
+
+}
